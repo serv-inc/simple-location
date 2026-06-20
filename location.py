d0b531286619a13256e2c2208acb455cc8761ece
Serv Inc <serv-inc@users.noreply.github.com>
2026-06-20 11:52:49 +0200

    lacked: saving and retrieving

---
+++ location.py
@@ -0,0 +1,60 @@
+#! /usr/bin/env python
+"""location set and retrieve"""
+import pathlib
+from dataclasses import dataclass
+from typing import Optional
+
+
+def user_dir() -> pathlib.Path:
+    return pathlib.Path.home() / ".config" / "location"
+
+
+def system_dir() -> pathlib.Path:
+    return pathlib.Path("/etc/location")
+
+
+@dataclass
+class Config:
+    lat: float
+    lon: float
+    alt: float = 0.0
+    path: Optional[pathlib.Path] = None
+
+    @property
+    def latitude(self) -> float:
+        return self.lat
+
+    @property
+    def longitude(self) -> float:
+        return self.lon
+
+    @property
+    def altitude(self) -> float:
+        return self.alt
+
+    @staticmethod
+    def from_dir(d: pathlib.Path) -> "Config":
+        lat = float((d / "latitude").open().read())
+        lon = float((d / "longitude").open().read())
+        try:
+            alt = float((d / "altitude").open().read())
+        except FileNotFoundError:
+            alt = 0.0
+        return Config(lat, lon, alt, path=d)
+
+    @staticmethod
+    def load() -> "Config":
+        if system_dir().exists():
+            return Config.from_dir(system_dir())
+        elif user_dir().exists():
+            return Config.from_dir(user_dir())
+        else:
+            return Config(0, 0, 0, None)  # 'Greenwich'
+
+    def save(self):
+        assert self.path
+        if not self.path.exists():
+            self.path.mkdir()
+        (self.path / "latitude").open("w").write(str(self.lat))
+        (self.path / "longitude").open("w").write(str(self.lon))
+        (self.path / "altitude").open("w").write(str(self.alt))
